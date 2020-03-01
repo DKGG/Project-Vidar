fd69d0701177c2a05a759d9697be4b148bf6b3c2
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2020-03-01 17:00:17 -0300

    Reordering scripts

--- Project Vidar/Assets/Scripts/Player/shadowFollow.cs
+++ Project Vidar/Assets/Scripts/Player/shadowFollow.cs
@@ -7,12 +7,9 @@ public class shadowFollow : MonoBehaviour
     public Transform target;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
-    public Light teste;
 
-    // Update is called once per frame
     void LateUpdate()
     {
-        
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
