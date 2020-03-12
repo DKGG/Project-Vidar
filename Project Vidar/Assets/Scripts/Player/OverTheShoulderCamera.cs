a9cfa9025c12a568a303f1093c0c7f49453aeb26
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2020-03-11 22:28:07 -0300

    Revert "Revert "Merge branch 'master' into Level-1-Blockage""
    This reverts commit 55cae14d0dccd93aeee79f5667bf421e41e709a9.

--- Project Vidar/Assets/Scripts/Player/OverTheShoulderCamera.cs
+++ Project Vidar/Assets/Scripts/Player/OverTheShoulderCamera.cs
@@ -9,7 +9,20 @@ public class OverTheShoulderCamera : MonoBehaviour
     [SerializeField] Transform playerTransForm, lookTarget;
     [SerializeField] float rotationSpeed = 5.0f;
 
+    [Header("Camera Zoom")]
+    [Range(0.0f, 10.0f)]
+    public float speed = 5.0f;
+
+    InputController inputController;
+    public Transform zoomIn;
+    public Transform zoomOut;
+
+    bool moveCam = false;
+
     private Transform cameraPivot;
+
+    private float startTime;
+    private float journeyLength;
 
     // Private variables
     private float mouseX, mouseY;
@@ -17,18 +30,45 @@ public class OverTheShoulderCamera : MonoBehaviour
     private void Start()
     {
         cameraPivot = transform.parent;
+        
+
+        startTime = Time.deltaTime;
+
+        journeyLength = Vector3.Distance(zoomOut.transform.position, zoomIn.transform.position);
+    }
+
+    private void Update()
+    {    
+                        
+        if (inputController.GetKeyX())
+        {
+          
+            moveCam = !moveCam;
+        }
+
+        if(moveCam == true)
+        {
+            transform.position = Vector3.Lerp(transform.position, zoomIn.transform.position, Time.deltaTime * 5f);
+            
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, zoomOut.transform.position, Time.deltaTime * 5f);
+        }
+
     }
 
     private void LateUpdate()
     {
-        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseX += inputController.CheckInputMouseX() * rotationSpeed;
+        mouseY -= inputController.CheckInputMouseY() * rotationSpeed;
 
         mouseY = Mathf.Clamp(mouseY, -80, 80);
 
         cameraPivot.position = playerTransForm.position;
 
-        transform.LookAt(lookTarget);
-        cameraPivot.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        // transform.LookAt(lookTarget);
+        cameraPivot.rotation = Quaternion.Euler(mouseY, mouseX, 0);       
+              
     }
 }
--- Project Vidar/Assets/Scripts/ThrowBox.cs
+++ Project Vidar/Assets/Scripts/ThrowBox.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ThrowContinuousBox : MonoBehaviour
+public class ThrowBox : MonoBehaviour
 {
     enum directionForce
     {
@@ -14,10 +14,11 @@ public class ThrowContinuousBox : MonoBehaviour
     public Transform checaChao;
 
     [SerializeField] directionForce dirf;
-    
+
     bool colidiu = false;
     bool isGrounded;
-    public bool push = false;
+    public bool push = false;    
+    public float strength;
 
     public string lockSide = "";
 
@@ -33,13 +34,20 @@ public class ThrowContinuousBox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("Push é: " + push);
-        //Debug.Log(colidiu);
-        //Debug.Log(lockSide);
+        Debug.Log("strength no throwBox" + strength);
         isGrounded = Physics.Linecast(checaChaoCenter.position, checaChao.position);
-
-        if (push == true && colidiu == false)
+        if (push==true)
         {
+            ApplyForce();
+        }
+
+    }
+
+    private void ApplyForce()
+    {
+        push = false;
+        //if (push == true)
+        //{
             if (dirf.Equals(directionForce.normal))
             {
                 switch (lockSide)
@@ -47,22 +55,22 @@ public class ThrowContinuousBox : MonoBehaviour
                     case "norte":
                         rb.constraints = RigidbodyConstraints.None;
                         rb.constraints = RigidbodyConstraints.FreezeRotation;
-                        rb.velocity = transform.right * 5000 * Time.deltaTime;
+                        rb.velocity = transform.right * strength /* *Time.deltaTime*/;
                         break;
                     case "sul":
                         rb.constraints = RigidbodyConstraints.None;
                         rb.constraints = RigidbodyConstraints.FreezeRotation;
-                        rb.velocity = -transform.right * 5000 * Time.deltaTime;
+                        rb.velocity = -transform.right * strength /* *Time.deltaTime*/;
                         break;
                     case "oeste":
                         rb.constraints = RigidbodyConstraints.None;
                         rb.constraints = RigidbodyConstraints.FreezeRotation;
-                        rb.velocity = -transform.forward * 5000 * Time.deltaTime;
+                        rb.velocity = -transform.forward * strength /* *Time.deltaTime*/;
                         break;
                     case "leste":
                         rb.constraints = RigidbodyConstraints.None;
                         rb.constraints = RigidbodyConstraints.FreezeRotation;
-                        rb.velocity = transform.forward * 5000 * Time.deltaTime;
+                        rb.velocity = transform.forward * strength /** Time.deltaTime*/;
                         break;
                     case "":
                         break;
@@ -76,39 +84,18 @@ public class ThrowContinuousBox : MonoBehaviour
             {
                 rb.constraints = RigidbodyConstraints.None;
                 rb.constraints = RigidbodyConstraints.FreezeRotation;
-                rb.velocity = transform.up * 5000 * Time.deltaTime;
+                rb.velocity = transform.up * 50  /* * Time.deltaTime*/;
             }
-        }
-        if (colidiu == true)
-        {
-            if (Mathf.Abs(rb.velocity.x) < 1 || Mathf.Abs(rb.velocity.y) < 1 || Mathf.Abs(rb.velocity.z) < 1)
-            {
-                Vector3 parar = new Vector3(0, Physics.gravity.y, 0);
-                rb.velocity = parar;
-                if (Mathf.Abs(rb.velocity.x) < 1 && Mathf.Abs(rb.velocity.y) < 1 && Mathf.Abs(rb.velocity.z) < 1)
-                {
-                    rb.velocity = Vector3.zero;
-                    push = false;
-                }
-
-            }
-        }
-
+        //}
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("paraBloco"))
-        {
-            colidiu = true;
-        }
+       
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            colidiu = false;
-        }
+        
     }
 }
