52bf8f99b09fc6611869a5c0319cc55272ced369
DKGG <50000510+DKGG@users.noreply.github.com>
2020-03-12 16:44:19 -0300

    Mudança de camera quando o Player locka na caixa
    Alternativa pra melhora da mecânica de lockar

--- Project Vidar/Assets/Scripts/Player/OverTheShoulderCamera.cs
+++ Project Vidar/Assets/Scripts/Player/OverTheShoulderCamera.cs
@@ -8,8 +8,15 @@ public class OverTheShoulderCamera : MonoBehaviour
     [Header("Look Properties")]
     [SerializeField] Transform playerTransForm, lookTarget;
     [SerializeField] float rotationSpeed = 5.0f;
+    [SerializeField] GameObject playerObject;
+    Transform OldTarget;
+
+    bool changeCam;
 
     public Transform cameraPivot;
+
+    Vector3 changeTargetAxis;
+    Vector3 OldchangeTargetAxis;
 
     // Private variables
     private float mouseX, mouseY;
@@ -17,16 +24,37 @@ public class OverTheShoulderCamera : MonoBehaviour
     private void Start()
     {
         cameraPivot = transform.parent;
+        changeTargetAxis = new Vector3(1.5f, 0.5f, -8f);
+        OldchangeTargetAxis = new Vector3(1.5f, 0.5f, -2.5f);
+        OldTarget = playerTransForm;
+    }
+
+    private void Update()
+    {
+        if (PlayerEntity.getLocked() == true)
+        {
+            Camera.main.transform.localPosition = Vector3.Lerp(Camera.main.transform.localPosition, changeTargetAxis, Time.deltaTime * 5);
+            playerTransForm = playerObject.transform.parent;
+            changeCam = true;
+        }
+        else if (PlayerEntity.getLocked() == false && changeCam == true)
+        {
+            Camera.main.transform.localPosition = Vector3.Lerp(Camera.main.transform.localPosition, OldchangeTargetAxis, Time.deltaTime * 100);
+            playerTransForm = OldTarget;
+            changeCam = false;
+        }
+
     }
 
     private void LateUpdate()
     {
-        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseX += PlayerEntity.checkMouseX() * rotationSpeed;
+        mouseY -= PlayerEntity.checkMouseY() * rotationSpeed;
 
         mouseY = Mathf.Clamp(mouseY, -80, 80);
 
         cameraPivot.position = playerTransForm.position;
+
 
         transform.LookAt(lookTarget);
         cameraPivot.rotation = Quaternion.Euler(mouseY, mouseX, 0);
