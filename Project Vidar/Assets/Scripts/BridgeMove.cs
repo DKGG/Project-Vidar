c4c15821addcbb06502f1962b9c8f663d976d2be
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2020-03-13 22:36:35 -0300

    Moving platform Rigidbody option and bridge position update

---
+++ Project Vidar/Assets/Scripts/BridgeMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeMove : MonoBehaviour
+{
+    [SerializeField] GameObject bridge;
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bridge.transform.position = new Vector3(-0.75f, -3.5f, -0.75f);
+        // rb.MovePosition(transform.position + forward * Time.fixedDeltaTime);
+    }
+}
