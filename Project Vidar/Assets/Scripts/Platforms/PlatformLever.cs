253adb1abcdf06bd1c8e2c1f94a175951f45e4a7
Martin Kretzschmar <martin@jp7.com.br>
2020-04-25 18:09:44 -0300

    Lever design and platforms update

---
+++ Project Vidar/Assets/Scripts/Platforms/PlatformLever.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformLever : MonoBehaviour
+{
+    [SerializeField] MovingPlatform platformTarget;
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Can be through trigger
+        if (Input.GetKeyDown(KeyCode.E) && !platformTarget.movementEnabled)
+            ActivatePlatform();
+
+        // if (Input.GetKeyDown(KeyCode.E) && platformTarget.movementEnabled)
+        //    DeactivatePlatform();
+    }
+
+    private void ActivatePlatform()
+    {
+        platformTarget.movementEnabled = true;
+    }
+
+    private void DeactivatePlatform()
+    {
+        platformTarget.movementEnabled = false;
+    }
+}
