a2bebae7a372339141fec8eea595287c887eaafe
Pedro Aredes <49043940+pedroaredes@users.noreply.github.com>
2020-05-08 19:40:55 -0300

    JumpChange

--- Project Vidar/Assets/Scripts/Player/playerJump.cs
+++ Project Vidar/Assets/Scripts/Player/playerJump.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class playerJump : MonoBehaviour
 {
     Rigidbody rb;
-    
+
     public float jumpForce = 5f;
-    public int jumpCounter = 0;
+    public int jumpCounter = 1;
     [SerializeField] private float dragDownForce = 5f;
     [SerializeField] private float extraGravity = 1.6f;
 
     [SerializeField] CapsuleCollider vidarSphere;
     [SerializeField] LayerMask groundLayers;
+    bool isGround2;
 
     private void Start()
-    {        
+    {
         rb = GetComponent<Rigidbody>();
     }
 
@@ -26,29 +27,24 @@ public class playerJump : MonoBehaviour
 
         if (PlayerEntity.getButtonJump())
         {
-            if (PlayerEntity.getGrounded() && jumpCounter > 0)
-            {
-                Jump();
-            }
-            else if (PlayerEntity.getCanDoubleJump() && jumpCounter > 0)
+            if (PlayerEntity.getGrounded() == true && jumpCounter > 0)
             {
                 Jump();
                 PlayerEntity.setCanDoubleJump(false);
             }
+            else if (PlayerEntity.getCanDoubleJump() && jumpCounter > 0)
+            {
+                Jump();
+                PlayerEntity.setCanDoubleJump(false); ;
+            }
         }
+
     }
 
     private void GroundCheck()
     {
-        PlayerEntity.setGrounded(
-            Physics.CheckCapsule(
-                vidarSphere.bounds.center,
-                new Vector3(vidarSphere.bounds.center.x, vidarSphere.bounds.min.y, vidarSphere.bounds.center.z),
-                vidarSphere.radius * .9f,
-                groundLayers)
-        );
+        PlayerEntity.setGrounded(Physics.CheckCapsule(vidarSphere.bounds.center, new Vector3(vidarSphere.bounds.center.x, vidarSphere.bounds.min.y, vidarSphere.bounds.center.z), vidarSphere.radius * .9f, groundLayers));
     }
-
     private void JumpBalance()
     {
         if (Mathf.Abs(rb.velocity.y) <= 1f && !PlayerEntity.getGrounded() && !PlayerEntity.getDashing())
@@ -56,31 +52,29 @@ public class playerJump : MonoBehaviour
             AnimatorManager.setStateFalling();
             PlayerEntity.setIsFalling(true);
             return;
-        }
 
+        }
         if (Mathf.Abs(rb.velocity.y) < 1)
         {
+
             rb.velocity += Vector3.up * Physics.gravity.y * (dragDownForce - 1) * Time.deltaTime;
         }
         else if (Mathf.Abs(rb.velocity.y) > 0 && !PlayerEntity.getButtonJump())
         {
+
             rb.velocity += Vector3.up * Physics.gravity.y * (extraGravity - 1) * Time.deltaTime;
         }
-
         if (PlayerEntity.getGrounded())
         {
-            jumpCounter = 2;
+            jumpCounter = 1;
             PlayerEntity.setIsFalling(false);
-        }
 
-        if (!PlayerEntity.getCanDoubleJump())
-            PlayerEntity.setCanDoubleJump(true);
+        }
     }
 
     private void Jump()
     {
         PlayerEntity.setJumping(true);
-        FindObjectOfType<AudioManager>().Play("jump");
         AnimatorManager.setStateJump();
         jumpCounter -= 1;
         //PlayerEntity.setCanPlayJumpAnim(true);
@@ -88,13 +82,12 @@ public class playerJump : MonoBehaviour
         StartCoroutine(Wait());
         StartCoroutine(secondJumpTimer());
     }
-
     private IEnumerator Wait()
     {
         yield return 1;
         PlayerEntity.setJumping(false);
-    }
 
+    }
     private IEnumerator secondJumpTimer()
     {
         yield return new WaitForSeconds(0.55f);
