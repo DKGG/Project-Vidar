cebdb6fb6f5960c54fd165bfedd17467e8d51ed2
João Barberino <42822732+joaopbarberino@users.noreply.github.com>
2020-05-21 01:35:04 -0300

    Remove Debug.Log

--- Project Vidar/Assets/Scripts/Dialogue System/DialogueManager.cs
+++ Project Vidar/Assets/Scripts/Dialogue System/DialogueManager.cs
@@ -28,7 +28,6 @@ public class DialogueManager : MonoBehaviour
     {
         if (PlayerEntity.getIsOnDialogue() && PlayerEntity.getButtonJump())
         {
-            Debug.Log("SOCORRO");
             DisplayNextSentence();
         }
     }
