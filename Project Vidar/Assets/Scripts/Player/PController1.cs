6f57f7adc56cb6112b006238db83edfc75d63599
DKGG <50000510+DKGG@users.noreply.github.com>
2020-06-01 20:35:52 -0300

    Código das caixas das caixas continuas  corrigidos

--- Project Vidar/Assets/Scripts/Player/PController1.cs
+++ Project Vidar/Assets/Scripts/Player/PController1.cs
@@ -10,19 +10,16 @@ public class PController1 : MonoBehaviour
     GameObject caixa = null;
     [SerializeField]GameObject player;
     Rigidbody rb;
-
-    // Start is called before the first frame update
+   
     void Start()
-    {
-        //PlayerEntity.setIdle(true);
+    {        
         rb = GetComponent<Rigidbody>();        
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        //Debug.Log(PlayerEntity.getLocked()+"Locked");
-        //Debug.Log(PlayerEntity.getIsLockedInContinuous() + "LockedinContinuous");
+        
 
         if (PlayerEntity.getBoxLocked() != null)
         {
@@ -36,8 +33,7 @@ public class PController1 : MonoBehaviour
             }
 
             if(PlayerEntity.getLocked() == true && PlayerEntity.getIsLockedInContinuous() == true && PlayerEntity.getKeyQ() == true)
-            {
-                //Debug.Log("Entrei no if que muda o estado do wantToThrow");
+            {                
                 PlayerEntity.setWantToThrow(true);                
             }
             
@@ -47,17 +43,7 @@ public class PController1 : MonoBehaviour
     }
 
     public void PlayerStatus()
-    {
-        //if (PlayerEntity.getIdle() == true)
-        //{
-        //    PlayerEntity.setLocked(false);
-        //    GetComponent<Movement>().enabled = true;
-        //    GetComponent<playerJump>().enabled = true;
-        //    GetComponent<Dash>().enabled = true;
-        //    GetComponent<RaycastShoot>().enabled = true;
-        //    rb.isKinematic = false;            
-        //}
-
+    {     
         if (PlayerEntity.getLocked() == true)
         {
             //PlayerEntity.setIdle(false);
@@ -69,7 +55,7 @@ public class PController1 : MonoBehaviour
 
             transform.position = Vector3.Lerp(transform.position, PlayerEntity.getPositionToLock().position, Time.deltaTime * 5f);
             transform.rotation = Quaternion.Lerp(transform.rotation, PlayerEntity.getPositionToLock().rotation, Time.deltaTime * 5f);
-            if (Mathf.Abs(Vector3.Distance(transform.position, PlayerEntity.getPositionToLock().position)) < 1f)
+            if (Mathf.Abs(Vector3.Distance(transform.position, PlayerEntity.getPositionToLock().position)) < 0.1f)
             {               
                 transform.position = PlayerEntity.getPositionToLock().position;                
             }
@@ -84,42 +70,5 @@ public class PController1 : MonoBehaviour
             rb.isKinematic = false;
         }
 
-    }
-
-    //private void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject.CompareTag("ContinuosBox"))
-    //    {
-    //        caixa = other.gameObject;
-    //        PlayerEntity.setBoxLocked(caixa);
-    //        PlayerEntity.setIsInside(true);
-    //        PlayerEntity.setIsInsideOfContinuous(true);
-    //    }
-
-    //    if (other.gameObject.CompareTag("SimpleBox"))
-    //    {
-    //        caixa = other.gameObject;
-    //        PlayerEntity.setBoxLocked(caixa);
-    //        PlayerEntity.setIsInside(true);
-    //        PlayerEntity.setIsInsideOfSimple(true);
-    //    }
-    //}
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.gameObject.CompareTag("ContinuosBox"))
-    //    {
-    //        caixa = null;
-    //        //PlayerEntity.setBoxLocked(caixa);
-    //        PlayerEntity.setIsInside(false);
-    //        PlayerEntity.setIsInsideOfContinuous(false);
-    //    }
-
-    //    if (other.gameObject.CompareTag("SimpleBox"))
-    //    {
-    //        caixa = null;
-    //        //PlayerEntity.setBoxLocked(caixa);
-    //        PlayerEntity.setIsInside(false);
-    //        PlayerEntity.setIsInsideOfSimple(false);
-    //    }
-    //}
+    }    
 }
