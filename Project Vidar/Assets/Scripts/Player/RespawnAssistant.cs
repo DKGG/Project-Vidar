70fe20763098db71c081c24aa5c63cd4e98d68fe
Martin Kretzschmar <martin@jp7.com.br>
2020-06-06 17:44:10 -0300

    Fog particles

--- Project Vidar/Assets/Scripts/Player/RespawnAssistant.cs
+++ Project Vidar/Assets/Scripts/Player/RespawnAssistant.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class RespawnAssistant : MonoBehaviour
 {
     Camera m_MainCamera;
+    OverTheShoulderCamera cameraScript;
 
     void Start()
     {
         //This gets the Main Camera from the Scene
         m_MainCamera = Camera.main;
+        cameraScript = m_MainCamera.GetComponent<OverTheShoulderCamera>();
     }
 
     void Update()
@@ -17,13 +19,17 @@ public class RespawnAssistant : MonoBehaviour
         if (transform.position.y < 0)
         {
             // Fade Out when falling
-
+            cameraScript.enabled = false;
             // ==
 
             // Move Player to Respawn Point
             // Debug.Log(PlayerEntity.getSpawnPoint());
             transform.position = PlayerEntity.getSpawnPoint();
             // ==
+        }
+        else if (!cameraScript.enabled)
+        {
+            cameraScript.enabled = true;
         }
     }
 }
