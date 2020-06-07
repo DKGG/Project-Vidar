87c7bb5fcbdf4db050d39758235b4276632ddd6e
João Barberino <42822732+joaopbarberino@users.noreply.github.com>
2020-06-07 19:17:45 -0300

    Add in game menu

---
+++ Project Vidar/Assets/Scripts/Menu/PauseController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public RectTransform ResumeButton;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Debug.Log("resume");
+        Cursor.visible = false;
+        // Cursor.lockState = CursorLockMode.Locked;
+
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.3f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        Debug.Log("pause");
+
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.visible = true;
+        // Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitMenu()
+    {
+        Debug.Log("sair");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
