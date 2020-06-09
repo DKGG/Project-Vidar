998779888bce9536125e1bce688c79f9578d1558
João Barberino <42822732+joaopbarberino@users.noreply.github.com>
2020-06-09 12:35:21 -0300

    Add powers UI, visual and management

---
+++ Project Vidar/Assets/Scripts/HUD/PowersManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowersManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject dashRune;
+    [SerializeField]
+    private GameObject strengthRune;
+    [SerializeField]
+    private GameObject doubleJumpRune;
+    [SerializeField]
+    private GameObject freezeRune;
+
+    private CanvasGroup dashCanvas;
+    private CanvasGroup strengthCanvas;
+    private CanvasGroup doubleJumpCanvas;
+    private CanvasGroup freezeCanvas;
+
+    private bool dashCorroutineStarted = false;
+    private bool strengthCorroutineStarted = false;
+    private bool doubleJumpCorroutineStarted = false;
+    private bool freezeCorroutineStarted = false;
+
+
+    private void Awake()
+    {
+        dashCanvas = dashRune.GetComponent<CanvasGroup>();
+        strengthCanvas = strengthRune.GetComponent<CanvasGroup>();
+        doubleJumpCanvas = doubleJumpRune.GetComponent<CanvasGroup>();
+        freezeCanvas = freezeRune.GetComponent<CanvasGroup>();
+    }
+
+    void Update()
+    {
+        if (!PlayerEntity.getGrounded() && PlayerEntity.getJumping() && !PlayerEntity.getIsOnDialogue() && !doubleJumpCorroutineStarted)
+        {
+            Debug.Log("mano socorro");
+            StartCoroutine(IncreaseAlpha(doubleJumpCanvas));
+        }
+        else if (PlayerEntity.getGrounded() && !PlayerEntity.getJumping())
+        {
+            Debug.Log("por favor");
+            StartCoroutine(DecreaseAlpha(doubleJumpCanvas));
+        }
+    }
+
+    IEnumerator IncreaseAlpha(CanvasGroup canvas)
+    {
+        doubleJumpCorroutineStarted = true;
+        while (canvas.alpha < 1)
+        {
+            canvas.alpha += 0.1f;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    IEnumerator DecreaseAlpha(CanvasGroup canvas)
+    {
+        while (canvas.alpha > 0.5f)
+        {
+            canvas.alpha -= 0.1f;
+            yield return new WaitForSeconds(0.1f);
+        }
+        doubleJumpCorroutineStarted = false;
+
+    }
+}
