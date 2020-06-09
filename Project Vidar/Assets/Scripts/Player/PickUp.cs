d0c57e49015467cfcdc05e68292275bbdb91e82a
Martin Kretzschmar <martin@jp7.com.br>
2020-06-09 09:46:55 -0300

    Fix PickUp object destination

--- Project Vidar/Assets/Scripts/Player/PickUp.cs
+++ Project Vidar/Assets/Scripts/Player/PickUp.cs
@@ -5,36 +5,34 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     [SerializeField] Transform grabPos;
-
     bool grab = false;
 
     private void OnTriggerStay(Collider other)
     {
-        // Can be through trigger
         if (Input.GetKeyDown(KeyCode.E) && !grab)
-            GrabAction(); // PickUpObject();
+            StartCoroutine(GrabAction()); // PickUpObject();
         else if (Input.GetKeyDown(KeyCode.E) && grab)
-            DropAction(); // DropObject();
+            StartCoroutine(DropAction()); // DropObject();
     }
 
-    private void GrabAction()
+    IEnumerator GrabAction()
     {
-        
+        yield return new WaitForEndOfFrame();
         GetComponent<Rigidbody>().useGravity = false;
         transform.position = grabPos.position;
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
         transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
         grab = true;
-        
+        yield return null;
     }
 
-    private void DropAction()
+    IEnumerator DropAction()
     {
-        
+        yield return new WaitForEndOfFrame();
         transform.parent = null;
         GetComponent<Rigidbody>().useGravity = true;
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
         grab = false;
-
+        yield return null;
     }
 }
