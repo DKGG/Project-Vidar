0e905a33b71979d3a0de1cfd5427fe4b859037fc
João Barberino <42822732+joaopbarberino@users.noreply.github.com>
2020-06-13 03:40:43 -0300

    Add checkpoint mark on interface and fix puzzle boundaries

--- Project Vidar/Assets/Scripts/Player/RespawnController.cs
+++ Project Vidar/Assets/Scripts/Player/RespawnController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class RespawnController : MonoBehaviour
 {
     private Vector3 spawnPoint;
+    [SerializeField]
+    private Animator animator;
 
     private void Start()
     {
@@ -15,8 +18,20 @@ public class RespawnController : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Player"))
         {
+            if (PlayerEntity.getSpawnPoint() != spawnPoint)
+            {
+                animator.SetBool("IsActive", true);
+                StartCoroutine(TurnOffAnimation());
+            }
             PlayerEntity.setSpawnPoint(spawnPoint);
+
             // col.transform.position = spawnPoint;
         }
+    }
+
+    IEnumerator TurnOffAnimation()
+    {
+        yield return new WaitForSeconds(3f);
+        animator.SetBool("IsActive", false);
     }
 }
