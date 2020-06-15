758d9e128bb4da1acc64c8c0f9f672c6d5e5c61f
Martin Kretzschmar <martin@jp7.com.br>
2020-06-15 17:04:02 -0300

    Level Changer

--- Project Vidar/Assets/Scripts/HUD/Menu/LevelChanger.cs
+++ Project Vidar/Assets/Scripts/HUD/Menu/LevelChanger.cs
@@ -1,12 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelChanger : MonoBehaviour
 {
     //public Animator animator;
     [SerializeField]
-    private int levelIndex;
-    
+    private int sceneIndex;
+
+    [SerializeField]
+    GameObject loadingScreen;
+    [SerializeField]
+    Slider slider;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -15,18 +22,29 @@ public class LevelChanger : MonoBehaviour
             PlayerEntity.setJumping(false);
             PlayerEntity.setWalking(false);
             AnimatorManager.setStateIdle();
-            FadeToLevel();
-            OnFadeComplete();
+            // FadeToLevel();
+            StartCoroutine(LoadAsynchronously(sceneIndex));
         }
     }
 
-    public void FadeToLevel ()
-    {        
+    public void FadeToLevel()
+    {
         //animator.SetTrigger("FadeOut");
     }
 
-    public void OnFadeComplete(){
-        
-        SceneManager.LoadScene(levelIndex);        
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        // SceneManager.LoadScene(levelIndex);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
     }
 }
