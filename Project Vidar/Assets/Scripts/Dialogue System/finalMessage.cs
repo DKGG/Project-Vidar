fb39e72d69040c4ada0afd94d0c32f51c84632d7
Martin Kretzschmar <martin@jp7.com.br>
2020-06-16 19:12:33 -0300

    Thanks Scene

---
+++ Project Vidar/Assets/Scripts/Dialogue System/finalMessage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finalMessage : MonoBehaviour
+{
+    [SerializeField]
+    public Animator anim;
+    [SerializeField]
+    public GameObject textOne;
+    [SerializeField]
+    public GameObject textTwo;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            textOne.SetActive(false);
+            textTwo.SetActive(false);
+            anim.SetBool("logoOn", true);
+
+            StartCoroutine(WaitForSecondsCall());
+        }
+    }
+
+    IEnumerator WaitForSecondsCall ()
+    {
+        yield return new WaitForSeconds(2f);
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
